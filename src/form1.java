e58985cbbcf5d6e31bb0a9c4814ee8c24892f254
Estefanía Sánchez <estefi2000ms2@gmail.com>
2024-02-02 11:21:53 -0500

    FORM 1

--- src/form1.java
+++ src/form1.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class form1 {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
 }
