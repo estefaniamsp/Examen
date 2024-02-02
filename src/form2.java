ea8b606030a7d162c56e49b2b6dc5f213554705b
Samuel Cuti <samcuti.03@gmail.com>
2024-02-02 11:25:14 -0500

    v2.0

--- src/form2.java
+++ src/form2.java
@@ -1,13 +1,12 @@
 import javax.swing.*;
 
-public class form1 {
+public class form2 {
+
+
     private JButton button1;
     private JButton button2;
     private JButton button3;
     private JButton button4;
     private JButton button5;
     private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    protected JPanel panel1;
 }
