317126c2430a785ba4576e9961996c0a271c0313
Estefanía Sánchez <estefi2000ms2@gmail.com>
2024-02-02 11:26:05 -0500

    NUMEROS

--- src/form1.java
+++ src/form1.java
@@ -1,13 +1,13 @@
 import javax.swing.*;
 
 public class form1 {
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a7Button;
+    private JButton a8Button;
     protected JPanel panel1;
 }
