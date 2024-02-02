e201f615f35c04a72386a5769ab02ac2e01af9a2
Samuel Cuti <samcuti.03@gmail.com>
2024-02-02 11:33:58 -0500

    v2.0

--- src/form1.java
+++ src/form1.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form1 {
     private JButton a1Button;
@@ -13,4 +15,88 @@ public class form1 {
     private JButton a9Button;
     private JButton a0Button;
     private JButton ENTER;
+    private JLabel labelC;
+
+    public form1() {
+
+
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a7Button.getText());
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a8Button.getText());
+
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a9Button.getText());
+
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a4Button.getText());
+
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a5Button.getText());
+
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a6Button.getText());
+
+            }
+        });
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a1Button.getText());
+
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a2Button.getText());
+
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a3Button.getText());
+
+            }
+        });
+        a0Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelC.setText(labelC.getText()+a0Button.getText());
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
 }
