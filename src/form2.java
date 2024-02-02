abca1909a6808bc83ded893fc6b55a0579fb33cb
Estefanía Sánchez <estefi2000ms2@gmail.com>
2024-02-02 11:52:19 -0500

    FORM SALDO

--- src/form2.java
+++ src/form2.java
@@ -3,11 +3,9 @@ import javax.swing.*;
 public class form2 {
 
 
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
+    private JButton verSaldoButton;
+    private JButton retiroButton;
+    private JButton depositoButton;
+    private JButton salirButton;
     protected JPanel panel2;
 }
